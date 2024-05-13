f19d2a5b4ef7ee11e6e8cf9090045070238ca1a8
eriktav <erik.tav1@gmail.com>
2024-05-13 13:41:24 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
 import streamlit as st
 st.title('Vamos aprenser sobre segurança em Helipontos de embarcações e plataformas marítimas')
 st.write('Normam 223!')
+from hugchat import hugchat
+from hugchat.login import Login
