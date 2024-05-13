029a80c9183aa55d01ea28707fa439b191094d6a
eriktav <erik.tav1@gmail.com>
2024-05-13 13:43:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,7 @@ st.title('Vamos aprenser sobre segurança em Helipontos de embarcações e plata
 st.write('Normam 223!')
 from hugchat import hugchat
 from hugchat.login import Login
+st.set_page_config(page_title="🤗💬 HugChat")
+# Store LLM generated responses
+if "messages" not in st.session_state.keys():
+    st.session_state.messages = [{"role": "assistant", "content": "Como posso ajudar?"}]
