e1009b0a43cb35b370106f7daa4e96dde9c7c651
eriktav <erik.tav1@gmail.com>
2024-05-17 16:57:40 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,8 +68,7 @@ for message in st.session_state.messages:
         st.write(message["content"])
 
 # Function for generating LLM response
-
-def generate_response(prompt_input):
+def generate_response(prompt_input, email, passwd):
     # Hugging Face Login
     sign = Login(email, passwd)
     cookies = sign.login()
@@ -78,16 +77,16 @@ def generate_response(prompt_input):
     return chatbot.chat(prompt_input)
 
 # User-provided prompt
-  if prompt := st.chat_input(disabled=yes):
-      st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message():
+if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
         st.write(prompt)
 
 # Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
-            response = generate_response(prompt) 
+            response = generate_response(prompt, hf_email, hf_pass) 
             st.write(response) 
     message = {"role": "assistant", "content": response}
     st.session_state.messages.append(message)
