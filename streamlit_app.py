7ac948210c99fc564fefb6891cdd487a4b7e01d7
eriktav <erik.tav1@gmail.com>
2024-05-17 16:55:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -78,7 +78,7 @@ def generate_response(prompt_input):
     return chatbot.chat(prompt_input)
 
 # User-provided prompt
-  if prompt := st.chat_input():
+  if prompt := st.chat_input(disabled=not (hf_email and hf_pass)):
       st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message():
         st.write(prompt)
