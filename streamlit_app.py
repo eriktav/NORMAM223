f2ad23f42a42024f69701a26fd4b097b7968890f
eriktav <erik.tav1@gmail.com>
2024-05-17 17:15:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,9 +81,9 @@ if prompt := st.chat_input(""):
 
 # Generate a new response if last message is not from assistant
     if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
+      with st.chat_message("assistant"):
         with st.spinner("Thinking..."):
             response = generate_response(prompt, hf_email, hf_pass) 
             st.write(response) 
-    message = {"role": "assistant", "content": response}
-    st.session_state.messages.append(message)
+      message = {"role": "assistant", "content": response}
+      st.session_state.messages.append(message)
